213577f15dd41d5d0a2f4413b667fe26d94d6b29
Abhinav Sancheti <001sancheti@gmail.com>
2021-08-25 20:54:10 +0530

    written all test cases with TDD approach

---
+++ src/Counter/Counter.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './Counter.css';
+
+const Counter = () => {
+    const [counterValue, setCounterValue] = useState(0);
+    const [inputValue, setInputValue] = useState(1);
+
+    const addToCounter = () => {
+        setCounterValue(counterValue + inputValue);
+    }
+
+    const subtractFromCounter = () => {
+        setCounterValue(counterValue - inputValue);
+    }
+
+    return (
+        <div>
+            <h2 data-testid='header'>My Counter</h2>
+            <h2 data-testid='counter' className={`${counterValue >= 100 ? "green": ""}${counterValue <= -100 ? "red": ""}`}>{counterValue}</h2>
+            <button 
+                data-testid='subtract-btn'
+                onClick={subtractFromCounter}
+                >-</button>
+            <input
+                data-testid='input'
+                type='number'
+                value={inputValue}
+                className="text-center"
+                onChange={(e) => {
+                    setInputValue(parseInt(e.target.value));
+                }}
+            />
+            <button 
+                data-testid='add-btn'
+                onClick={addToCounter}
+            >+</button>
+        </div>
+    );
+};
+
+export default Counter;
